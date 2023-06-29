fa0856390e85297f8e4a3032d13d20be3914c193
Andrés Weitzel <andres96energy@hotmail.com>
2023-06-29 17:59:00 -0300

    Fixxed dotenv

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
+//External
+require('dotenv').config();
+//Environment vars
+const PORT = process.env.APP_PORT || 8082;
+//Config
 const { appMiddleware } = require("./config/middleware/index");
+//Const-vars
+let app;
 
+
+/**
+ * @description función encargada de levantar el servidor, añadir la configuración inicial y setear las rutas http
+ * @returns active instance of the server for handling requests and responses
+ */
 const run = async() => {
-    
-  const app = await appMiddleware();
+
+  app = await appMiddleware();
 
   app.get("/", (req, res) => {
     res.json({ mesagge: "Hello" });
   });
-
-  const PORT = process.env.PORT || 8080;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
